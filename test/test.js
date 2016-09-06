1f163e9b58db5f30997d5d03569214366bc7e943
Alejandro Ruiz <alexrwave@gmail.com>
2016-09-06 12:39:57 +0200

    Use bitnami-gulp-common-tasks (#8)

--- test/test.js
+++ test/test.js
@@ -36,7 +36,7 @@ describe('Logger', function() {
       // This might be broken, if you pass logFileLevel on it's own, getLogFileLevel returns -1, as
       // it is returning the minimum of this.level and this.logFileLevel, however, shouldn't it be
       // returning 1 (since it is the default level) if that's the case?
-      const l = new Logger({ level: 'error', logFileLevel: 'error' });
+      const l = new Logger({level: 'error', logFileLevel: 'error'});
       expect(l.level).to.be('error');
     });
   });
@@ -62,7 +62,7 @@ describe('Logger', function() {
       expect(runIntercepted(() => l.log('DEBUG', 'hello', 'world')))
         .to.be('');
 
-      l = new Logger({ level: 'error' });
+      l = new Logger({level: 'error'});
       expect(runIntercepted(() => l.log('INFO', 'hello', 'world')))
         .to.be('');
     });
@@ -72,19 +72,19 @@ describe('Logger', function() {
       expect(runIntercepted(() => l.error('hello world')))
         .to.match(/hello world/);
 
-      l = new Logger({ level: 'silent' });
+      l = new Logger({level: 'silent'});
       expect(runIntercepted(() => l.error('hello world'))).to.be('');
     });
 
     it('logs to file if log file specified when Logger is initialized', function(done) {
       const tmpFile = tmp.tmpNameSync();
-      const l = new Logger({ logFile: tmpFile });
+      const l = new Logger({logFile: tmpFile});
 
       expect(runIntercepted(() => l.info('hello', 'world')))
         .to.be(`${colors[levelsConfig.colors.info]('INFO ')} hello world\n`);
 
       setTimeout(function() {
-        expect(fs.readFileSync(tmpFile, { encoding: 'utf8' })).to.match(/INFO\s+hello world\n$/);
+        expect(fs.readFileSync(tmpFile, {encoding: 'utf8'})).to.match(/INFO\s+hello world\n$/);
         fs.unlinkSync(tmpFile);
         done();
       }, 200);
@@ -92,13 +92,13 @@ describe('Logger', function() {
 
     it('can log different levels for file and stdout', function(done) {
       const tmpFile = tmp.tmpNameSync();
-      const l = new Logger({ logFile: tmpFile, level: 'error', fileLogLevel: 'info' });
+      const l = new Logger({logFile: tmpFile, level: 'error', fileLogLevel: 'info'});
 
       expect(runIntercepted(() => l.info('hello world')))
         .to.be('');
 
       setTimeout(function() {
-        expect(fs.readFileSync(tmpFile, { encoding: 'utf8' })).to.match(/INFO\s+hello world\n$/);
+        expect(fs.readFileSync(tmpFile, {encoding: 'utf8'})).to.match(/INFO\s+hello world\n$/);
         fs.unlinkSync(tmpFile);
         done();
       }, 200);
@@ -117,7 +117,7 @@ describe('DelegatedLogger', function() {
       expect(runIntercepted(() => wrappedLogger.info('hello world')))
         .to.be(`${colorize('wrapped', 'white')} ${colorize('INFO ', levelsConfig.colors.info)} hello world\n`);
       setTimeout(function() {
-        expect(fs.readFileSync(tmpFile, { encoding: 'utf8' })).to.match(/INFO\s+hello world\n$/);
+        expect(fs.readFileSync(tmpFile, {encoding: 'utf8'})).to.match(/INFO\s+hello world\n$/);
         fs.unlinkSync(tmpFile);
         done();
       }, 200);
